f24fcbcbea3283dc10a51a96b48ceda03e1c67af
David Flanderka <t426.001@centrum.cz>
2020-04-09 15:16:50 +0200

    Merge files.
    - merge merge header files of  handler with test cc files

--- test/test_exprtk.cc
+++ test/test_exprtk.cc
@@ -9,10 +9,64 @@
 #define TEST_EXPRTK_CC_
 
 #include <string>
+#include <cstdlib>
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <ios>
+#include <iomanip>
+#include <numeric>
 
-#include "exprtk_handler.hh"
+#include "base_handler.hh"
+#include "exprtk.hpp"
 #include "time_profiler.hh"
+
+class ExprtkParserHandler : public BaseHandler {
+public:
+	ExprtkParserHandler() : BaseHandler() {}
+
+    void init(int vec_size) override {
+    	this->create_data_vectors(vec_size);
+    }
+
+    double parse_vector_fast(std::string expr, std::string tag_name) override {
+        typedef exprtk::symbol_table<double> symbol_table_t;
+        typedef exprtk::expression<double>     expression_t;
+        typedef exprtk::parser<double>             parser_t;
+
+        std::string expression_string = " result_vec := " + expr + " ";
+
+        exprtk::vector_view<double> x_view = exprtk::make_vector_view(x_v,x_v.size());
+        exprtk::vector_view<double> y_view = exprtk::make_vector_view(y_v,y_v.size());
+        exprtk::vector_view<double> z_view = exprtk::make_vector_view(z_v,z_v.size());
+        exprtk::vector_view<double> r_view = exprtk::make_vector_view(result_v,result_v.size());
+
+        double sum = 0.0;
+
+        symbol_table_t symbol_table;
+        symbol_table.add_vector("x",x_view);
+        symbol_table.add_vector("y",y_view);
+        symbol_table.add_vector("z",z_view);
+        symbol_table.add_vector("result_vec",r_view);
+        symbol_table.add_constants();
+
+        expression_t expression;
+        expression.register_symbol_table(symbol_table);
+
+        parser_t parser;
+        parser.compile(expression_string,expression);
+
+        START_TIMER(nBulkSize, tag_name);
+        for (int j=0; j<nLoops; ++j) {
+            expression.value();
+        }
+        END_TIMER(nBulkSize, tag_name);
+        for (int i=0; i<nBulkSize; ++i) sum += result_v[i];
+
+        return sum;
+    }
+
+};
 
 /**
  * Function tests of base expressions parsing in exprtk:
--- test/test_muparser.cc
+++ test/test_muparser.cc
@@ -8,14 +8,56 @@
 #ifndef TEST_MUPARSER_CC_
 #define TEST_MUPARSER_CC_
 
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
 #include <string>
 #include <iostream>
+#include <limits>
+#include <ios>
+#include <iomanip>
+#include <numeric>
 
-#include "muparser_handler.hh"
+#include "base_handler.hh"
+#include "muParserTest.h"
 #include "time_profiler.hh"
 
+using namespace std;
 using namespace mu;
 
+
+class MuParserHandler : public BaseHandler {
+public:
+	MuParserHandler() : BaseHandler() {
+	    complexLine = "2*x + y*3 + x*(z-y) + 2*_pi*z";
+	    powerLine = "2*x + y^3 + x*(z-y) + 2*_pi*z";
+	    funcSin = "sin(_pi*y)";
+	}
+
+    void init(int vec_size) override {
+        create_data_vectors(vec_size);
+    }
+
+    double parse_vector_fast(std::string expr, std::string tag_name) override {
+        value_type sum = 0.0;
+
+        mu::Parser  parser;
+        parser.DefineVar( _T("x"), &(x_v[0]) );
+        parser.DefineVar( _T("y"), &(y_v[0]) );
+        parser.DefineVar( _T("z"), &(z_v[0]) );
+        parser.SetExpr(expr);
+
+        START_TIMER(nBulkSize, tag_name);
+        for (int j=0; j<nLoops; ++j) {
+            parser.Eval(&(result_v[j]), nBulkSize);
+        }
+        END_TIMER(nBulkSize, tag_name);
+        for (int i=0; i<nBulkSize; ++i) sum += result_v[i];
+
+        return double(sum);
+    }
+
+};
 
 /**
  * Function tests of parsing in exprtk:
